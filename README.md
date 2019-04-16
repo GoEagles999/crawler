fe4b7cb3a34593e2891c73a74d42cb1236df3e2e
Richard Demeny <richard.demeny@tigerspike.com>
2019-04-16 20:29:08 +0100

    complete

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # crawler
+I ran out of time
+
+# How it works
+This crawler is implemented in Node.js and traverses the DOM looking for the following elements:
+- anchor tags (<a href='asd' />)
+- img tags (<img src='asd' />)
+
+# Tradeoffs made
+When crawling pages it does not look for redirects implemented in JavaScript code ie assignment statements to document.href or window.location object, or jQuery code. In addition, sites may contain self-closing or not self-closing tags. The crawler supports self-closing image and anchor tags only.
+
+# How to build
+Run 'npm install' in root directory 
+
+# How to run 
+Run crawler with Node.js v10 on a domain: npm start wipro.com
+Run unit tests: npm run test
---
+++ crawler.js
@@ -0,0 +1,91 @@
+
+const axios = require('axios')
+const entryPoint = process.argv[2]
+
+const staticAssets = []
+const urls = []
+// match every <img> tag in order to extract value of 'src' property
+const imgTag = new RegExp('<img','gi')
+// match every <a> tag in order to extract value of 'href' propery
+const anchorTag = new RegExp('<a','gi') 
+const hrefAttr = new RegExp('href')
+const srcAttr = new RegExp('src')
+// placeholder for storing position of matched DOM node
+let result 
+
+const crawl = async url => {
+    // holds DOM as string
+    let document
+    try {
+        document = await axios.get(url)
+        document = document.data
+    } catch (e) {
+        console.log(`Error occured fetching ${url}'s resources`, e)
+    }
+    while(result = imgTag.exec(document)) {
+        // assume 'src' property of <img> tag is in the next 200 characters
+        // TODO: check if there is a /> string marking end of img tag
+        // TODO: construct another RegExp object and search for the closing tag in candidate to get its' position
+        const candidate = document.substr(result.index, 200)
+        // copy every character into an array
+        const split = candidate.split('')
+        const positions = []
+        split.forEach((char, i) => {
+            if (char == "'" || char == '"') {
+                // store position of all " or ' characters in the candidate string
+                positions.push(i)
+            }
+        })
+        positions.forEach((position, i) => {
+            // if the character before the occurence ' or " by position 3 is 'c' that means we found the sr*c* attribute
+            if (candidate[position-2] == 'c' && candidate[position-3] == 'r') {
+                // this position has an src attribute, get ready for extraction of the value!
+                const beginPositionForValue = positions[i]
+                // the next " or ' character marks end of the value
+                const endPositionForValue = positions[i+1]
+                // we have the href value
+                const srcvalue = candidate.substr(beginPositionForValue, endPositionForValue)
+                staticAssets.push(srcvalue)
+            }
+        })
+    }
+    while(result = anchorTag.exec(document)) {
+        // assume 'href' property of <img> tag is in the next 200 characters
+        // TODO: check if there is a /> string marking end of img tag
+        // TODO: construct another RegExp object and search for the closing tag in candidate to get its; position
+        const candidate = document.substr(result.index, 200)
+        // copy every character into an array
+        const split = candidate.split('')
+        const positions = []
+        split.forEach((char, i) => {
+            if (char == "'" || char == '"') {
+                // store position of all " or ' characters in the candidate string
+                positions.push(i)
+            }
+        })
+        positions.forEach((position, i) => {
+            // if the character before the occurence ' or " by position 3 is 'f' that means we found the hre*f* attribute
+            if (candidate[position-2] == 'f' && candidate[position-3] == 'e') {
+                // this position has a href attribute, get ready for extraction of the value!
+                const beginPositionForValue = positions[i]
+                // the next " or ' character marks end of the value
+                const endPositionForValue = positions[i+1]
+                // we have the href value
+                const hrefvalue = candidate.substr(beginPositionForValue, endPositionForValue)
+                urls.push(hrefvalue)
+            }
+        })
+    }
+    // TODO repeat the crawling process for each page
+    // from root resource on host
+    urls.forEach(url => {
+        crawl(url)
+    })
+} 
+
+crawl(`https://${entryPoint}`)
+
+// TODO delete duplicates in array
+console.log(`Plain text: \n ${urls.join('\n')}`)
+// TODO generate XML
+console.log(`XML: \n`)
